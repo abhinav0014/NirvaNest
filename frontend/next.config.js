4b44cf820f79da7b91cecfdccf132303d73ed665
Abhinav Gyawali <104615184+abhinav0014@users.noreply.github.com>
2026-01-03 12:22:44 +0545

    Update Next.js config with new settings
    Added configuration options for output, base path, and images.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,23 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} 
 const nextConfig = {
   reactStrictMode: true,
 }
 
 module.exports = nextConfig
+*/
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  output: "export",
+
+  basePath: "/NirvaNest",
+  assetPrefix: "/NirvaNest/",
+
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
